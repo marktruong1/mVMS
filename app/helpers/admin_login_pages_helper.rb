2e4f691406f06fad7da91181f9f634b3949673b1
Ziko <zikoku07@gmail.com>
2019-03-27 01:22:44 +0600

    Fixed issue for empty content

--- app/helpers/admin_login_pages_helper.rb
+++ app/helpers/admin_login_pages_helper.rb
@@ -1,9 +1,18 @@
 module AdminLoginPagesHelper
     def section_1_image(page_content)
-      page_content.section_1_image.blank? ? '/images/microhealth-logo.svg' : page_content&.section_1_image&.url
+      if page_content.present?
+        page_content.section_1_image.blank? ? '/images/microhealth-logo.svg' : page_content&.section_1_image&.url
+      else
+        '/images/microhealth-logo.svg'
+      end
+
     end
   
     def section_2_image(page_content)
-      page_content.section_2_image.blank? ? '/images/micro-health-icon.png' : page_content&.section_2_image&.url
+      if page_content.present?
+        page_content.section_2_image.blank? ? '/images/micro-health-icon.png' : page_content&.section_2_image&.url
+      else
+        '/images/micro-health-icon.png'
+      end
     end
 end
